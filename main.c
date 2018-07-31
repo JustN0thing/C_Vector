a3109a82664f026cc768ad89996cb784d9e75575
Just_n0thing <vampir9911@gmail.com>
2018-07-31 03:05:42 +0300

    Example of using

--- main.c
+++ main.c
@@ -8,12 +8,16 @@ int main()
 
     CVector_Init(&vect);
 
-    CVector_PushBack(&vect,12);
+    CVector_PushBack(&vect,122);
     CVector_PushBack(&vect,15);
     CVector_PushBack(&vect,25);
    CVector_PushBack(&vect,45);
-    CVector_Add(&vect,500,13); 
-     CVector_Add(&vect,200,14); 
+    CVector_PushBack(&vect,45);
+    CVector_Add(&vect,0xc1,13); 
+    CVector_Add(&vect,200,14); 
+    CVector_Add(&vect,200,24); 
+     CVector_Add(&vect,5000,56); 
+    CVector_Add(&vect,2000,54); 
      
    
 
