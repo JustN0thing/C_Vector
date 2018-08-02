a6dd4a0a2fdba365e83bd153a6717b0802939a72
Just_n0thing <vampir9911@gmail.com>
2018-08-02 04:32:05 +0300

    ...

--- main.c
+++ main.c
@@ -4,18 +4,22 @@
 int main()
 {
     int a [12];
-    CVector vect;
-    
-    CVector_Init(&vect);
-    CVector_PushBack(&vect,120);
-    CVector_Assign(&vect,10,150);
-    CVector_PushBack(&vect,220);
+    CVector vect,vv;
+    void** arr;
+
   
+    CVector_Init(&vect);
+    CVector_Init(&vv);
+   CVector_PushBack(&vect,5);
+    CVector_Assign(&vect,10,10);
+    CVector_PushBack(&vect,20);
+    
+    CVector_Assign(&vv,15,250);
    
-        printf("Capacity:%d\n",CVector_Capacity(&vect));
-        printf("Size:%d\n",CVector_Size(&vect));
+    CVector_Swap(&vv,&vect);
    
-        CVector_CopyToArray(&vect,&a);
+      
+        CVector_ToArray(&vv,&a);
 
         for(int i = 0; i<12;i++)
         printf("a[%d]:=%d\n",i,a[i]);
